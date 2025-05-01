7009986aeec4b48cc899061da8c3ace84ca2c27a
DominicDev <s0l0estudi4r@gmail.com>
2025-05-01 16:21:26 +0200

    v0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-![n1](preview/n1.avif)
+[![Foto Preview](preview/n60.avif)](https://20essentials.github.io/project-000-060)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-000-059" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-000-061" target="_blank">&#8594;</a>
+</div>
--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const d = document,
   w = window;
 
-console.log("Background Reference ->", "https://wallhaven.cc/w/yxo3yd");
-
-let createRaindrops = (amount) => {
+let createRaindrops = amount => {
   let i = 0;
 
   while (i < amount) {
-    let container = d.createElement("article"),
-      raindrop = d.createElement("div"),
-      duration = Math.random() * 4 + 1 + "s",
-      delay = Math.random() * -20 + "s",
-      scale = Math.random() * 0.1 + 0.5,
-      posX = Math.random() * w.innerWidth + "px";
+    let container = d.createElement('article'),
+      raindrop = d.createElement('div'),
+      duration = Math.random() * 4 + 1 + 's',
+      delay = Math.random() * -20 + 's',
+      scale = Math.random() * 0.1 + 0.3,
+      posX = Math.random() * w.innerWidth + 'px';
 
     container.style.left = posX;
     container.style.animationDuration = duration;
@@ -26,4 +24,4 @@ let createRaindrops = (amount) => {
   }
 };
 
-createRaindrops(222);
+createRaindrops(350);
